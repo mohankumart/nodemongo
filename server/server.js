0ac850271f273b312f592ef79b1b30f7ea9b369c
Mohan Kumar <tmk_mohankumar@yahoo.com>
2018-04-09 01:35:02 +0530

    set port for heroku

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var {Todo} = require('./models/todo')
 var {User} = require('./models/user')
 
 var app = express()
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -50,8 +51,8 @@ app.get('/todos/:id', (req, res)=>{
 })
 
 
-app.listen(3000,()=>{
-    console.log('Started server on port 3000')
+app.listen(port,()=>{
+    console.log(`Started server on port ${port}`)
 })
 
 
