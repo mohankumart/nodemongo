b324bbb068d024dd164e35b2dc108e5d4cbcd868
Mohan Kumar <tmk_mohankumar@yahoo.com>
2018-04-08 22:37:58 +0530

    server files

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+var {mongoose} = require('./db/mongoose')
+var {Todo} = require('./models/todo')
+var {User} = require('./models/user')
+
+var app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.post('/todos',(req, res)=>{
+    var todo = new Todo({
+        text: req.body.text
+    })
+
+    todo.save().then((doc)=>{
+        res.send(doc)
+    },(e)=>{
+        res.send(e)
+    })
+})
+
+app.get('/todos/:id', (req, res)=>{
+
+})
+
+
+app.listen(3000,()=>{
+    console.log('Started server on port 3000')
+})
