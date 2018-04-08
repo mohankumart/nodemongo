37631b506e0897db107d35c8654cc1dcb70d1e1a
Mohan Kumar <tmk_mohankumar@yahoo.com>
2018-04-08 17:45:50 +0530

    added delete

---
+++ playground/mongo-delete.js
@@ -0,0 +1,26 @@
+// const MongoClient = require('mongodb').MongoClient
+const {MongoClient, ObjectID} = require('mongodb')
+
+MongoClient.connect('mongodb://localhost:27017',(err, client)=>{
+    if(err){
+        console.log('unable to connect MongoDB server')
+    }else{
+        console.log('Connected to Mongo db server')
+    }
+
+    let db = client.db('TodoApp')  
+
+    //deleteMany
+    db.collection('Todos').deleteMany({text:'party'}).then((result)=>{
+        console.log(result)
+        client.close()
+    })
+    //deleteOne
+
+    //findOneAndDelete
+    db.collection('Todos').findOneAndDelete({'completed':false}).then((result)=>{
+        console.log(result)
+    })
+    
+    
+})
