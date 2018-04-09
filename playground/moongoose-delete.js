6685dc3e1c984f9edf62e6bb5a3ead18525f5cb3
Mohan Kumar <tmk_mohankumar@yahoo.com>
2018-04-09 12:25:06 +0530

    delete functions

---
+++ playground/moongoose-delete.js
@@ -0,0 +1,18 @@
+const {ObjectID} = require('mongodb')
+
+const {mongoose} = require('./../server/db/mongoose')
+const {Todo} = require('./../server/models/todo')
+const {User} = require('./../server/models/user')
+
+Todo.remove({}).then((res)=>{
+    console.log(res)
+},(err)=>{
+
+})
+
+Todo.findOneAndRemove('123').then((res)=>{
+    console.log(res)
+},(err)=>{
+    console.log(err)
+})
+
